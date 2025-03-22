4ad908af6d10350231f3933b380c376ad9e61c73
Alex Legkovsky <legkovsky.alexei@yandex.ru>
2025-03-22 22:09:48 +0300

    left only English for the exercises

--- 1_binary_search.py
+++ 1_binary_search.py
@@ -19,20 +19,9 @@ print(binary_search(my_list, 3))
 print(binary_search(my_list, -1))
 
 
-# ”пражнени€
 # Exercises
 
 '''
-RU
-1.1 »меетс€ отсортированный список из 128 имен, и вы ищете в нем значение методом бинарного поиска. 
- акое максимальное количество проверок дл€ этого может потребоватьс€?
-–ешение:
-ћаксимальное количество проверок= log2(n)
-n - количество элементов в списке
-log2(128) = 7
-ќтвет: 7 проверок
-
-EN
 1.1 You have a sorted list of 128 names and you are searching the list for a value using the binary search method. 
 What is the maximum number of checks this may require?
 Solution:
@@ -41,16 +30,9 @@ n is the number of items in the list
 log2(128) = 7
 Answer: 7 checks
 
-RU
-1.2 ѕредположим, размер списка увеличилс€ вдвое.  ак изменитс€ максимальное количество проверок?
-новый размер списка = 256
-log2(256) = 8
-ќтвет: максимальное количество проверок увеличитс€ до 8
-
-EN
 1.2 Suppose the size of the list is doubled. How will the maximum number of checks change?
 new list size = 256
 log2(256) = 8
-Answer: the maximum number of checks will increase to 8.
+Answer: the maximum number of checks will increase to 8
 
 '''
